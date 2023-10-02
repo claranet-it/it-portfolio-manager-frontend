e16c949c8a633aa814569315f7cd1d681b916585
gioboa <giorgiob.boa@gmail.com>
2023-10-02 17:16:29 +0200

    add profile fetch

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,14 +1,34 @@
-import { component$, useTask$ } from '@builder.io/qwik';
+import { $, component$, useTask$ } from '@builder.io/qwik';
 import { auth0 } from '../app';
 
 export const Profile = component$(() => {
 	useTask$(async () => {
 		try {
-			const token = await auth0.getTokenSilently();
-			console.log(token);
+			const token = await auth0.getIdTokenClaims();
+			if (token) {
+				console.log(token.__raw);
+			}
 		} catch (error: unknown) {
 			throw error;
 		}
 	});
-	return <>Profile</>;
+
+	const getUser = $(async () => {
+		const token = 'abc';
+		const response = await fetch(
+			`${import.meta.env.VITE_BACKEND_URL}/api/user/me/`,
+			{
+				method: 'GET',
+				headers: new Headers({ Authorization: `Basic ${token}` }),
+			}
+		);
+		console.log(response, 'response');
+	});
+
+	return (
+		<>
+			Profile
+			<button onClick$={getUser}>Get User</button>
+		</>
+	);
 });
