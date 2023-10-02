3658f7b2cef220795fbf580c53ace3121f295b9b
gioboa <giorgiob.boa@gmail.com>
2023-10-02 18:35:28 +0200

    manage envs

--- src/app.tsx
+++ src/app.tsx
@@ -1,21 +1,26 @@
 import { Auth0Client } from '@auth0/auth0-spa-js';
 import { component$ } from '@builder.io/qwik';
 import { Router, RouterConfig, initRouter } from 'qwik-router';
+import { Auth } from './components/Auth';
 import { Home } from './components/Home';
 import { Login } from './components/Login';
 import { Profile } from './components/Profile';
 
+const {
+	VITE_AUTH_DOMAIN: domain,
+	VITE_AUTH_CLIENTID: clientId,
+	VITE_AUTH_REDIRECT_URI: redirect_uri,
+	VITE_AUTH_AUDIENCE: audience,
+} = import.meta.env;
 export const auth0 = new Auth0Client({
-	domain: 'iam.clara.net',
-	clientId: 'S8a16tq1dnnqsUPr5q8x7zRUHgQgDVpT',
-	authorizationParams: {
-		redirect_uri: 'https://main.dh81w1al6xzzw.amplifyapp.com',
-		audience: 'https://it-portfolio-api',
-	},
+	domain,
+	clientId,
+	authorizationParams: { redirect_uri, audience },
 });
 
 const routes: RouterConfig = [
 	{ path: '/', component: Home },
+	{ path: '/auth', component: Auth },
 	{ path: '/login', component: Login },
 	{ path: '/profile', component: Profile },
 ];
