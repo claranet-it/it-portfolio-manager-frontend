8f327bd137ca885ecbe25b52381e0b069a5a45f6
gioboa <giorgiob.boa@gmail.com>
2023-10-02 18:39:50 +0200

    fix build

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -4,7 +4,7 @@ import { setCookie } from '../utils/cookie';
 export const Auth = component$(() => {
 	useVisibleTask$(async () => {
 		const url = new URL(window.location.href);
-		const code = url.searchParams.get('code');
+		url.searchParams.get('code');
 		setCookie('token', 'abc');
 		// if (code) {
 		// 	const token = await auth0.getIdTokenClaims();
