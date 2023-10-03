4bbcf9e46d43a555d16a1c6b1d9ebbb2a021ae9f
gioboa <giorgiob.boa@gmail.com>
2023-10-03 09:31:06 +0200

    change auth logic

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -1,18 +1,20 @@
 import { component$, useVisibleTask$ } from '@builder.io/qwik';
+import { auth0 } from '../app';
 import { setCookie } from '../utils/cookie';
 
 export const Auth = component$(() => {
 	useVisibleTask$(async () => {
 		const url = new URL(window.location.href);
-		url.searchParams.get('code');
-		setCookie('token', 'abc');
-		// if (code) {
-		// 	const token = await auth0.getIdTokenClaims();
-		// 	if (token) {
-		// setCookie('token', token.__raw);
-		// 	}
-		// 	window.location.href = '/';
-		// }
+		const code = url.searchParams.get('code');
+		console.log('code', code);
+		if (code) {
+			const token = await auth0.getIdTokenClaims();
+			console.log('code', JSON.stringify(token));
+			if (token) {
+				setCookie('token', token.__raw);
+			}
+			window.location.href = '/';
+		}
 	});
 	return <div />;
 });
