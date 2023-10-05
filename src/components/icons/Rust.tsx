b5abd9c22aa7a306a6dab70a59824d9f19cedb60
gioboa <giorgiob.boa@gmail.com>
2023-10-05 16:32:17 +0200

    add Rust cmp

---
+++ src/components/icons/Rust.tsx
@@ -0,0 +1,19 @@
+import { component$ } from '@builder.io/qwik';
+
+export const Rust = component$(() => {
+	return (
+		<svg
+			xmlns='http://www.w3.org/2000/svg'
+			width='32'
+			height='32'
+			viewBox='0 0 15 15'
+		>
+			<path
+				fill='none'
+				stroke='currentColor'
+				stroke-linejoin='round'
+				d='M2 10.5h5m-4.5-6H9a1.5 1.5 0 1 1 0 3H4.5m0-3v6m3-3h1a2 2 0 0 1 2 2a1 1 0 0 0 1 1H13M7.5.5l1.344 1.11l1.693-.417l.73 1.584l1.706.359l-.03 1.743l1.382 1.063L13.54 7.5l.784 1.558l-1.382 1.063l.03 1.743l-1.707.359l-.729 1.584l-1.693-.418L7.5 14.5l-1.344-1.11l-1.693.417l-.73-1.584l-1.706-.359l.03-1.743L.675 9.058L1.46 7.5L.675 5.942L2.057 4.88l-.03-1.743l1.706-.359l.73-1.584l1.693.417L7.5.5Z'
+			/>
+		</svg>
+	);
+});
