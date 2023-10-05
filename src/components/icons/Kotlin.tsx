7b8a4a51f6947ecb46265253164d6f8a5e9b8dd7
gioboa <giorgiob.boa@gmail.com>
2023-10-05 16:31:05 +0200

    add Kotlin cmp

---
+++ src/components/icons/Kotlin.tsx
@@ -0,0 +1,21 @@
+import { component$ } from '@builder.io/qwik';
+
+export const Kotlin = component$(() => {
+	return (
+		<svg
+			xmlns='http://www.w3.org/2000/svg'
+			width='32'
+			height='32'
+			viewBox='0 0 24 24'
+		>
+			<path
+				fill='none'
+				stroke='currentColor'
+				stroke-linecap='round'
+				stroke-linejoin='round'
+				stroke-width='2'
+				d='M20 20H4V4h16M4 20L20 4M4 12l8-8m0 8l8 8'
+			/>
+		</svg>
+	);
+});
