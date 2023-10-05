b3c0139dc8e3c68f1850bd326a1a023e7ca6aa6b
gioboa <giorgiob.boa@gmail.com>
2023-10-05 16:32:33 +0200

    add icons

---
+++ src/components/icons/index.tsx
@@ -0,0 +1,100 @@
+import { AWS } from './AWS';
+import { Android } from './Android';
+import { Ansible } from './Ansible';
+import { Billing } from './Billing';
+import { CSharp } from './CSharp';
+import { Design } from './Design';
+import { Dynamo } from './Dynamo';
+import { Elixir } from './Elixir';
+import { Flutter } from './Flutter';
+import { Grafana } from './Grafana';
+import { IoT } from './Iot';
+import { JavaScript } from './JavaScript';
+import { Kafka } from './Kafka';
+import { Kotlin } from './Kotlin';
+import { Kubernetes } from './Kubernetes';
+import { Lambda } from './Lamda';
+import { Linux } from './Linux';
+import { PHP } from './PHP';
+import { Python } from './Python';
+import { QuickSight } from './QuickSight';
+import { Redshift } from './Redshift';
+import { Ruby } from './Ruby';
+import { Rust } from './Rust';
+import { TypeScript } from './TypeScript';
+import { IOS } from './iOS';
+
+export const getIcon = (skill: string) => {
+	switch (skill) {
+		case 'C# - Backend':
+			return <CSharp />;
+		case 'Cloud Finance (Billing e Cost explorer)':
+			return <Billing />;
+		case 'Cloud Governance (Control Tower)':
+			return <AWS />;
+		case 'Elixir - Backend':
+			return <Elixir />;
+		case 'IAC (Terraform, Cloudformation, CDK, Ansible)':
+			return <Ansible />;
+		case 'Java/Kotlin - Backend':
+			return <Kotlin />;
+		case 'JavaScript/TypeScript - Backend':
+			return <TypeScript />;
+		case 'JavaScript/TypeScript - Frontend':
+			return <JavaScript />;
+		case 'Machine Learning (Amazon Sagemaker, Rekognition, Lex..)':
+			return <AWS />;
+		case 'Monitoring (Cloudwatch, New Relic, Prometheus, Grafana)':
+			return <Grafana />;
+		case 'Multiplatform Mobile (ionic, react-native, flutter, etc etc)':
+			return <Flutter />;
+		case 'Native Android':
+			return <Android />;
+		case 'Native iOS':
+			return <IOS />;
+		case 'OS Server (Linux, Windows)':
+			return <Linux />;
+		case 'PHP - Backend':
+			return <PHP />;
+		case 'Programmazione (Bash, Python)':
+			return <TypeScript />;
+		case 'Python - Backend':
+			return <Python />;
+		case 'Ruby (Rails)':
+			return <Ruby />;
+		case 'Rust - Backend':
+			return <Rust />;
+		case 'Serverless (AWS Lambda, DynamoDB, Step Function...)':
+			return <Dynamo />;
+		case 'Serverless (Serverless Framework, AWS SAM)':
+			return <AWS />;
+		case 'Servizi CI/CD (codepipeline, codebuild, codedeploy)':
+			return <AWS />;
+		case 'Servizi Container Orchestration (ECS, EKS, Kubernetes)':
+			return <Kubernetes />;
+		case 'Servizi Data Analytics (Glue, Athena, Redshift, EMR)':
+			return <Redshift />;
+		case 'Servizi Data Migration (DMS, DataSync, Transfer Family)':
+			return <AWS />;
+		case 'Servizi Data Streaming (Kinesis, MSK, Kafka)':
+			return <Kafka />;
+		case 'Servizi Data Visualization (QuickSight, ...)':
+			return <QuickSight />;
+		case 'Servizi IoT':
+			return <IoT />;
+		case 'Servizi Migration (CloudEndure, SMS)':
+			return <AWS />;
+		case 'Servizi Networking (VPN, Transit Gateway, VPC Advanced)':
+			return <TypeScript />;
+		case 'Servizi Security (IAM advanced, KMS, HSM, WAF)':
+			return <AWS />;
+		case 'Servizi Serverless (Lambda, APIGW, Step Functions, DynamoDB)':
+			return <Lambda />;
+		case 'Servizi core (IAM, EC2,VPC,RDS,S3, Elasticache)':
+			return <TypeScript />;
+		case 'UI Development (HTML/CSS/SCSS, etc etc)':
+			return <Design />;
+		default:
+			return <AWS />;
+	}
+};
