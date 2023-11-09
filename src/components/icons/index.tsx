e42ea9e9c5814fe2d9e0cfa0495289d0e1492011
Francesco Strazzullo <francesco.strazzullo86@gmail.com>
2023-11-09 21:41:19 +0100

    new cloud icons

--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -28,13 +28,9 @@ export const getIcon = (skill: string) => {
 	switch (skill) {
 		case 'C#':
 			return <CSharp />;
-		case 'Cloud Finance (Billing e Cost explorer)':
-			return <Billing />;
-		case 'Cloud Governance (Control Tower)':
-			return <AWS />;
 		case 'Elixir':
 			return <Elixir />;
-		case 'IAC (Terraform, Cloudformation, CDK, Ansible)':
+		case 'IaC':
 			return <Ansible />;
 		case 'Java/Kotlin':
 			return <Kotlin />;
@@ -42,56 +38,32 @@ export const getIcon = (skill: string) => {
 			return <TypeScript />;
 		case 'Frontend (JS/TS)':
 			return <JavaScript />;
-		case 'Machine Learning (Amazon Sagemaker, Rekognition, Lex..)':
+		case 'ML':
 			return <AWS />;
-		case 'Monitoring (Cloudwatch, New Relic, Prometheus, Grafana)':
-			return <Grafana />;
 		case 'Multiplatform Mobile (ionic, react-native, flutter)':
 			return <Flutter />;
 		case 'Native Android':
 			return <Android />;
 		case 'Native iOS':
 			return <IOS />;
-		case 'OS Server (Linux, Windows)':
-			return <Linux />;
 		case 'PHP':
 			return <PHP />;
-		case 'Programmazione (Bash, Python)':
-			return <TypeScript />;
 		case 'Python':
 			return <Python />;
 		case 'Ruby (Rails)':
 			return <Ruby />;
 		case 'Rust':
 			return <Rust />;
-		case 'Serverless (AWS Lambda, DynamoDB, Step Function...)':
-			return <Dynamo />;
-		case 'Serverless (Serverless Framework, AWS SAM)':
-			return <AWS />;
-		case 'Servizi CI/CD (codepipeline, codebuild, codedeploy)':
-			return <AWS />;
-		case 'Servizi Container Orchestration (ECS, EKS, Kubernetes)':
+		case 'Container':
 			return <Kubernetes />;
-		case 'Servizi Data Analytics (Glue, Athena, Redshift, EMR)':
-			return <Redshift />;
-		case 'Servizi Data Migration (DMS, DataSync, Transfer Family)':
-			return <AWS />;
-		case 'Servizi Data Streaming (Kinesis, MSK, Kafka)':
-			return <Kafka />;
-		case 'Servizi Data Visualization (QuickSight, ...)':
+		case 'Data':
 			return <QuickSight />;
-		case 'Servizi IoT':
-			return <Raspberry />;
-		case 'Servizi Migration (CloudEndure, SMS)':
+		case 'Networking':
 			return <AWS />;
-		case 'Servizi Networking (VPN, Transit Gateway, VPC Advanced)':
-			return <TypeScript />;
-		case 'Servizi Security (IAM advanced, KMS, HSM, WAF)':
+		case 'Security':
 			return <AWS />;
-		case 'Servizi Serverless (Lambda, APIGW, Step Functions, DynamoDB)':
+		case 'Serverless':
 			return <Lambda />;
-		case 'Servizi core (IAM, EC2,VPC,RDS,S3, Elasticache)':
-			return <TypeScript />;
 		case 'UI Development (HTML/CSS/SCSS)':
 			return <Design />;
 		default:
