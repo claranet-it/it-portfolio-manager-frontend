8d388fa367d2dbaaaf08a6c83ad4c2607b6ca3dd
alefiori <alefiori97@gmail.com>
2024-01-10 14:23:27 +0100

    refactor: function rename checkStatusRequest -> checkHttpResponseStatus

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,34 +1,34 @@
-import { checkStatusRequest, getHttpResponse } from './http-requests';
+import { checkHttpResponseStatus, getHttpResponse } from './http-requests';
 import {
-    Configuration,
-    Effort,
-    Month,
-    SetUserProfile,
-    Skill,
-    SkillMatrix,
-    UserMe,
+	Configuration,
+	Effort,
+	Month,
+	SetUserProfile,
+	Skill,
+	SkillMatrix,
+	UserMe,
 } from './types';
 
 export const getUserMe = async (): Promise<UserMe> =>
-    getHttpResponse<UserMe>('user/me');
+	getHttpResponse<UserMe>('user/me');
 
 export const setUserProfile = async (body: SetUserProfile): Promise<boolean> =>
-    checkStatusRequest('user/profile', 201, 'POST', body);
+	checkHttpResponseStatus('user/profile', 201, 'POST', body);
 
 export const getConfiguration = async (): Promise<Configuration> =>
-    getHttpResponse<Configuration>('configuration');
+	getHttpResponse<Configuration>('configuration');
 
 export const getSkillMatrixMine = async (): Promise<Skill[]> =>
-    getHttpResponse<Skill[]>('skill-matrix/mine');
+	getHttpResponse<Skill[]>('skill-matrix/mine');
 
 export const pathSkillMatrixMine = async (skill: Skill): Promise<boolean> =>
-    checkStatusRequest('skill-matrix/mine', 204, 'PATCH', skill);
+	checkHttpResponseStatus('skill-matrix/mine', 204, 'PATCH', skill);
 
 export const getSkills = async (company: string = 'it'): Promise<SkillMatrix> =>
-    getHttpResponse<SkillMatrix>(`skill-matrix?company=${company}`);
+	getHttpResponse<SkillMatrix>(`skill-matrix?company=${company}`);
 
 export const getEffort = async (): Promise<Effort> =>
-    getHttpResponse<Effort>('effort');
+	getHttpResponse<Effort>('effort');
 
 export const putEffort = async (uid: string, month: Month): Promise<boolean> =>
-    checkStatusRequest('effort', 204, 'PUT', { uid, ...month });
+	checkHttpResponseStatus('effort', 204, 'PUT', { uid, ...month });
--- src/utils/http-requests.ts
+++ src/utils/http-requests.ts
@@ -4,45 +4,45 @@ import { getCookie } from './cookie';
 type HttpMethods = 'GET' | 'POST' | 'PUT' | 'PATCH';
 
 const getHeaders = () => {
-    const token = getCookie(COOKIE_TOKEN_KEY);
-    return new Headers({
-        Authorization: `Bearer ${token}`,
-        'Content-Type': 'application/json',
-    });
+	const token = getCookie(COOKIE_TOKEN_KEY);
+	return new Headers({
+		Authorization: `Bearer ${token}`,
+		'Content-Type': 'application/json',
+	});
 };
 
 const executeRequest = async (
-    path: string,
-    method: HttpMethods = 'GET',
-    body?: Object
+	path: string,
+	method: HttpMethods = 'GET',
+	body?: Object
 ) => {
-    const headers = getHeaders();
-    const options: RequestInit = {
-        method,
-        headers,
-        body: JSON.stringify(body),
-    };
-    return await fetch(
-        `${import.meta.env.VITE_BACKEND_URL}/api/${path}`,
-        options
-    );
+	const headers = getHeaders();
+	const options: RequestInit = {
+		method,
+		headers,
+		body: JSON.stringify(body),
+	};
+	return await fetch(
+		`${import.meta.env.VITE_BACKEND_URL}/api/${path}`,
+		options
+	);
 };
 
 export const getHttpResponse = async <Response>(
-    path: string,
-    method?: HttpMethods,
-    body?: Object
+	path: string,
+	method?: HttpMethods,
+	body?: Object
 ): Promise<Response> => {
-    const response = await executeRequest(path, method, body);
-    return response.status === 200 ? await response.json() : null;
+	const response = await executeRequest(path, method, body);
+	return response.status === 200 ? await response.json() : null;
 };
 
-export const checkStatusRequest = async (
-    path: string,
-    expectedStatus: number = 204,
-    method?: HttpMethods,
-    body?: Object
+export const checkHttpResponseStatus = async (
+	path: string,
+	expectedStatus: number = 204,
+	method?: HttpMethods,
+	body?: Object
 ): Promise<boolean> => {
-    const { status } = await executeRequest(path, method, body);
-    return status === expectedStatus;
+	const { status } = await executeRequest(path, method, body);
+	return status === expectedStatus;
 };
