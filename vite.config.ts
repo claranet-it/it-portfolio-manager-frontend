73fa1d8d6ad06107be55709483e7b7d4ab4dec93
alefiori <alefiori97@gmail.com>
2024-01-22 17:43:41 +0100

    feat: add vite-plugin-pwa to implement service worker for caching

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
-import { defineConfig } from 'vite'
-import { qwikVite } from '@builder.io/qwik/optimizer'
+import { defineConfig } from 'vite';
+import { qwikVite } from '@builder.io/qwik/optimizer';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    qwikVite({
-      csr: true,
-    }),
-  ],
-})
+	plugins: [
+		qwikVite({
+			csr: true,
+		}),
+		VitePWA({
+			registerType: 'autoUpdate',
+		}),
+	],
+});
