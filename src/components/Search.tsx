c1636357ca92a905cd567d249c200e73b95683b9
Giorgio Mandolini <giorgio.mandolini@it.clara.net>
2024-02-05 11:42:22 +0100

    fix: ui improvements

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -14,8 +14,12 @@ export const Search = component$(() => {
 		searchValueSig.value = '';
 		loadingSig.value = true;
 		chatStore.push({ question, answer: '...' });
-		const { message } = await openAI(question);
-		chatStore[chatStore.length - 1].answer = message;
+		try {
+			const { message } = await openAI(question);
+			chatStore[chatStore.length - 1].answer = message;
+		} catch (e) {
+			searchValueSig.value = question;
+		}
 		loadingSig.value = false;
 	});
 
@@ -23,7 +27,7 @@ export const Search = component$(() => {
 		<section class='max-w-[800px] mx-auto'>
 			{chatStore.map(({ answer, question }, index) => (
 				<section key={index}>
-					<div class='flex flex-col leading-1.5 p-4 my-2 border-gray-200 bg-gray-100 rounded-s-xl rounded-ee-xl w-3/4 whitespace-pre-line ml-auto'>
+					<div class='flex flex-col leading-1.5 p-4 my-2 border-gray-200 bg-red-100 rounded-s-xl rounded-ee-xl w-3/4 whitespace-pre-line ml-auto'>
 						<p class='text-sm font-normal text-gray-900'>{question}</p>
 					</div>
 					<div class='flex flex-col leading-1.5 p-4 my-2 border-gray-200 bg-gray-100 rounded-e-xl rounded-es-xl w-3/4 whitespace-pre-line'>
@@ -31,22 +35,36 @@ export const Search = component$(() => {
 					</div>
 				</section>
 			))}
-			<form class='text-center' onSubmit$={onSubmit} preventdefault:submit>
-				<textarea
-					disabled={loadingSig.value}
-					id='message'
-					class='w-full mt-8 block p-2.5 text-sm text-gray-900 bg-gray-50 rounded-lg border-2 border-red-500'
-					placeholder={t('search_placeholder')}
-					rows={6}
-					bind:value={searchValueSig}
-				></textarea>
-				<button
-					disabled={!searchValueSig.value || loadingSig.value}
-					type='submit'
-					class='bg-transparent text-gray-400 font-semibold p-2 m-2 hover:bg-red-600 hover:text-white rounded border-0 min-w-[100px] disabled:cursor-not-allowed disabled:hover:bg-transparent disabled:hover:text-gray-400 disabled:text-gray-400 disabled:opacity-50'
-				>
-					{t('search_button')}
-				</button>
+			<form onSubmit$={onSubmit} preventdefault:submit class='my-8'>
+				<label for='chat' class='sr-only'>
+					{t('search_placeholder')}
+				</label>
+				<div class='flex items-center px-3 py-2 rounded-lg bg-gray-50'>
+					<textarea
+						disabled={loadingSig.value}
+						id='chat'
+						rows={3}
+						class='block mx-4 p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-red-500 focus:border-red-500'
+						placeholder={t('search_placeholder')}
+						bind:value={searchValueSig}
+					></textarea>
+					<button
+						type='submit'
+						class='inline-flex justify-center p-2 text-red-600 rounded-full cursor-pointer hover:bg-red-100'
+						disabled={!searchValueSig.value || loadingSig.value}
+					>
+						<svg
+							class='w-5 h-5 rotate-90 rtl:-rotate-90'
+							aria-hidden='true'
+							xmlns='http://www.w3.org/2000/svg'
+							fill='currentColor'
+							viewBox='0 0 18 20'
+						>
+							<path d='m17.914 18.594-8-18a1 1 0 0 0-1.828 0l-8 18a1 1 0 0 0 1.157 1.376L8 18.281V9a1 1 0 0 1 2 0v9.281l6.758 1.689a1 1 0 0 0 1.156-1.376Z' />
+						</svg>
+						<span class='sr-only'>{t('search_button')}</span>
+					</button>
+				</div>
 			</form>
 		</section>
 	);
