253ed2285b8b9a3e4f3f60b1635581079ed4fefd
francesco-strazzullo <francesco.strazzullo86@gmail.com>
2024-04-17 22:03:58 +0200

    fix: logout not working

--- src/utils/cookie.ts
+++ src/utils/cookie.ts
@@ -30,6 +30,6 @@ export const getCookie = (name: string) => {
 };
 
 export const removeCookie = (name: string, cb: Function = () => { }) => {
-	document.cookie = name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+	document.cookie = name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;domain=.claranet.it';
 	window.requestAnimationFrame(() => cb());
 };
