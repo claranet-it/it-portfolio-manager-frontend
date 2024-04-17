d35783c70228e42afad068ee6e4e69f6eeb905b9
francesco-strazzullo <francesco.strazzullo86@gmail.com>
2024-04-17 18:34:17 +0200

    fix: logout on next tick

--- src/utils/cookie.ts
+++ src/utils/cookie.ts
@@ -31,5 +31,5 @@ export const getCookie = (name: string) => {
 
 export const removeCookie = (name: string, cb: Function) => {
 	document.cookie = name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-	cb();
+	window.requestAnimationFrame(() => cb());
 };
