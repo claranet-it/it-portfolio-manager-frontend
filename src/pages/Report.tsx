eeb6478750f6f07a6c912d09faea5b2215614f70
Francesco Mariani <francesco.mariani@claranet.com>
2024-06-25 14:17:58 +0200

    included filters to report page

--- src/pages/Report.tsx
+++ src/pages/Report.tsx
@@ -1,9 +1,28 @@
-import { component$ } from '@builder.io/qwik';
+import { component$, useSignal } from '@builder.io/qwik';
+import { ReportFilters } from 'src/components/ReportFilters';
+import { DataRange } from 'src/components/form/DataRange';
+import { useGetTimeSheetDays } from 'src/hooks/timesheet/useGetTimeSheetDays';
 
 export const Report = component$(() => {
+	const { from, to, nextWeek, prevWeek } = useGetTimeSheetDays();
+
+	const selectedCustomerSig = useSignal<string>('');
+	const selectedProjectSig = useSignal<string>('');
+	const selectedTaskSig = useSignal<string>('');
+	const selectedNameSig = useSignal<string>('');
+
 	return (
 		<div class='w-full px-3 pt-2.5 space-y-6'>
-			<p>Report</p>
+			<div class='flex flex-row item-right'>
+				<ReportFilters
+					selectedCustomer={selectedCustomerSig}
+					selectedProject={selectedProjectSig}
+					selectedTask={selectedTaskSig}
+					selectedName={selectedNameSig}
+				/>
+
+				<DataRange from={from} to={to} nextAction={nextWeek} prevAction={prevWeek} />
+			</div>
 		</div>
 	);
 });
