d92cf2fee73abf5f65332fea1e252906f63027f8
andreasz97 <andreas.szekely@it.clara.net>
2024-07-22 11:13:28 +0200

    wip: fixing duplicated time entries in timesheet

--- src/components/form/TimePicker.tsx
+++ src/components/form/TimePicker.tsx
@@ -1,8 +1,8 @@
-import { QRL, Signal, component$, useComputed$, useSignal } from '@builder.io/qwik';
+import { QRL, component$, useComputed$, useSignal } from '@builder.io/qwik';
 import { getIcon } from '../icons';
 
 interface TimePickerProps {
-	bindValue?: Signal<string> | undefined;
+	bindValue?: string | undefined;
 	onClick$?: QRL;
 	onChange$?: QRL;
 	onBlur$?: QRL;
@@ -11,7 +11,8 @@ interface TimePickerProps {
 }
 
 export const TimePicker = component$<TimePickerProps>(
-	({ bindValue = useSignal('00:00'), onClick$, onChange$, onBlur$, required, disabled }) => {
+	({ bindValue = '00:00', onClick$, onChange$, onBlur$, required, disabled }) => {
+		const signalValue = useSignal(bindValue);
 		const style = useComputed$(() => {
 			return disabled
 				? 'bg-darkgray-50 text-darkgray-300 border-darkgray-100'
@@ -33,7 +34,7 @@ export const TimePicker = component$<TimePickerProps>(
 					id='time'
 					class={`block w-full py-3 px-3 leading-none border text-sm rounded-md ${style.value} mr-1.5`}
 					min='00:00'
-					bind:value={bindValue}
+					bind:value={signalValue}
 					onChange$={onChange$}
 					onBlur$={onBlur$}
 					disabled={disabled}
