828b725a3aa92350657cf0240152cd3697855392
Francesco Strazzullo <francesco.strazzullo86@gmail.com>
2024-07-24 20:03:40 +0200

    refactor: removed moment (#65)
    * refactor: removed moment
    
    * fix day name format

--- src/hooks/timesheet/useGetTimeSheetDays.ts
+++ src/hooks/timesheet/useGetTimeSheetDays.ts
@@ -1,50 +1,41 @@
 import { $, useSignal, useTask$ } from '@builder.io/qwik';
-import moment from 'moment';
 import { Day } from '../../models/timeEntry';
+import {
+	currentWeek as currentWeekAsRange,
+	getDaysInRange,
+	nextWeek as nextWeekAsRange,
+	prevWeek as prevWeekAsRange,
+} from '../../utils/dates';
 
 export const useGetTimeSheetDays = () => {
 	// INITIALIZZATION
-	const _moment = moment();
-	const from = useSignal<Date>(_moment.startOf('isoWeek').toDate());
-	const to = useSignal<Date>(_moment.endOf('isoWeek').toDate());
+	const range = currentWeekAsRange();
+	const from = useSignal<Date>(range.from);
+	const to = useSignal<Date>(range.to);
 	const days = useSignal<Day[]>([]);
 
 	const updateDays = $(() => {
-		days.value = []; // empty the days list
-		const startDate = moment(from.value);
-		const endDate = moment(to.value);
-		let currentDate = startDate.clone();
-		while (currentDate.isSameOrBefore(endDate)) {
-			days.value.push({
-				name: currentDate.format('ddd').toString(),
-				date: currentDate.toDate(),
-				weekend: currentDate.isoWeekday() > 5,
-			});
-			// Move to the next day
-			currentDate.add(1, 'day');
-		}
+		days.value = [...getDaysInRange(from.value, to.value)];
 	});
 
 	const nextWeek = $(() => {
-		const _moment = moment(from.value);
-		_moment.add(1, 'week');
-		from.value = _moment.startOf('isoWeek').toDate();
-		to.value = _moment.endOf('isoWeek').toDate();
+		const range = nextWeekAsRange(from.value);
+		from.value = range.from;
+		to.value = range.to;
 		updateDays();
 	});
 
 	const prevWeek = $(() => {
-		const _moment = moment(from.value);
-		_moment.subtract(1, 'week');
-		from.value = _moment.startOf('isoWeek').toDate();
-		to.value = _moment.endOf('isoWeek').toDate();
+		const range = prevWeekAsRange(from.value);
+		from.value = range.from;
+		to.value = range.to;
 		updateDays();
 	});
 
 	const currentWeek = $(() => {
-		const _moment = moment();
-		from.value = _moment.clone().startOf('isoWeek').toDate();
-		to.value = _moment.clone().endOf('isoWeek').toDate();
+		const range = currentWeekAsRange();
+		from.value = range.from;
+		to.value = range.to;
 		updateDays();
 	});
 
