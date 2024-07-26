871d78975f8c052df5b54dc0549329c905fb099f
Francesco Mariani <francesco.mariani@claranet.com>
2024-07-26 11:49:47 +0200

    wip: change color for timesheet weekend days

--- src/components/TimeEntryElement.tsx
+++ src/components/TimeEntryElement.tsx
@@ -26,7 +26,7 @@ export const TimeEntryElement = component$<TimeEntryElementProps>(
 		const formattedDate = formatDateString(day.date);
 		const hours = entry ? timeEntriesState[entry.hours]?.[formattedDate] || 0 : 0;
 		const { weekend } = day;
-		const tdClass = `relative py-3 px-4 text-center border border-surface-50 ${weekend ? 'bg-danger-light' : ''}`;
+		const tdClass = `relative py-3 px-4 text-center border border-surface-50 ${weekend ? 'bg-surface-20' : ''}`;
 
 		const destriptionSig = useSignal(entry?.description ?? '');
 		const hoursSig = useSignal(entry?.hours ?? 0);
