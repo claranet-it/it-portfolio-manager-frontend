13b153df1883ce341835915a4bee8a188a845497
francesco-strazzullo <francesco.strazzullo86@gmail.com>
2024-07-31 17:34:11 +0200

    refactor: new modal mecanism for new customer/projects

--- src/components/modals/NewProjectModal.tsx
+++ src/components/modals/NewProjectModal.tsx
@@ -1,33 +1,19 @@
-import { $, component$, Slot, useComputed$, useSignal } from '@builder.io/qwik';
-import { t } from 'src/locale/labels';
-import { getIcon } from '../icons';
+import { component$, Slot, useComputed$ } from '@builder.io/qwik';
 
-export const NewProjectModal = component$(() => {
-	const modalVisible = useSignal(false);
+interface NewTimeEntryModalProps {
+	show: boolean;
+}
 
-	const modalToggle = $(() => {
-		modalVisible.value = !modalVisible.value;
-	});
-
+export const NewProjectModal = component$<NewTimeEntryModalProps>(({ show }) => {
 	const modalStyle = useComputed$(() => {
 		return {
-			display: modalVisible.value ? 'block' : 'none',
+			display: show ? 'block' : 'none',
 		};
 	});
 
 	return (
 		<>
-			<div class='w-full flex flex-row'>
-				<button id='open-new-project-bt' onClick$={modalToggle} type='button'>
-					<div class='flex flex-row space-x-1 content text-clara-red'>
-						<span class='text-xl content-center'>{getIcon('Add')}</span>
-						<span class='text-base font-bold content-center'>
-							{t('add_new_project_label')}
-						</span>
-					</div>
-				</button>
-			</div>
-			{modalVisible.value && (
+			{show && (
 				<div
 					id='default-modal'
 					class={`${modalStyle.value} overflow-y-auto overflow-x-hidden top-0 left-0 z-50 m-0 flex justify-center items-center w-full h-full bg-black-trasparent`}
