062bbf1d4bc70e43960c7964a7c589c2ff716e39
mariateresagraziano <maria.teresa.graziano@it.clara.net>
2025-03-21 10:14:11 +0100

    refactor type

--- src/hooks/curriculum/useCurriculumVitae.ts
+++ src/hooks/curriculum/useCurriculumVitae.ts
@@ -21,43 +21,6 @@ export const useCurriculumVitae = () => {
 
 		try {
 			curriculumVitae.value = await getCurriculumByEmail(email);
-			/* curriculumVitae.value = {
-				name: 'Maria Teresa Graziano',
-				email: 'maria.teresa.graziano@claranet.com',
-				role: 'Frontend Developer',
-				summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-				main_skills: 'Agile, Management, jira',
-				education: [
-					{
-						id: '1',
-						note: "Master's degree",
-						institution: 'University',
-						year_start: 2015,
-						year_end: 2020,
-						current: false,
-					},
-				],
-				work: [
-					{
-						id: '2',
-						note: 'R&D',
-						role: 'developer',
-						institution: 'Company',
-						year_start: 2020,
-						year_end: 2025,
-						current: false,
-					},
-					{
-						id: '3',
-						note: 'Ricercatore',
-						role: 'stage',
-						institution: 'Company X',
-						year_start: 2017,
-						year_end: 2020,
-						current: false,
-					},
-				],
-			}; */
 		} catch (error) {
 			const { message } = error as Error;
 			addEvent({
