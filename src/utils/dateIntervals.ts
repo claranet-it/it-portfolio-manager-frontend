671f1e00c6aeaa8435da210c1eef79fe828078d6
mariateresagraziano <marytex.dev@gmail.com>
2025-06-23 09:29:16 +0200

    fix date comparing

--- src/utils/dateIntervals.ts
+++ src/utils/dateIntervals.ts
@@ -8,5 +8,8 @@ export const intervalIntersect = (startTemplate: Date, endTemplate: Date, from: 
 };
 
 export const isInInterval = (day: Date, from: Date, to: Date) => {
-	return day >= from && day <= to;
+	const dayWithoutTime = new Date(day.getFullYear(), day.getMonth(), day.getDate());
+	const fromWithoutTime = new Date(from.getFullYear(), from.getMonth(), from.getDate());
+	const toWithoutTime = new Date(to.getFullYear(), to.getMonth(), to.getDate());
+	return dayWithoutTime >= fromWithoutTime && dayWithoutTime <= toWithoutTime;
 };
