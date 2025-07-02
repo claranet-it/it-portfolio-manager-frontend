bfcfdcace4aa1978024253c0eb779446aec805e7
mariateresagraziano <marytex.dev@gmail.com>
2025-07-02 15:05:33 +0200

    remove comments

--- src/components/company/CompanySkills.tsx
+++ src/components/company/CompanySkills.tsx
@@ -9,29 +9,6 @@ type Props = {
 };
 export const CompanySkills = component$<Props>(({ company, updateSkillVisibility }) => {
 	const _companySkills = useStore(company.value.skills);
-	/* const skills = useSignal([
-		{ id: 'id1', name: 'skill1', visible: true },
-		{ id: 'id2', name: 'skill2', visible: true },
-		{ id: 'id3', name: 'skill3', visible: true },
-	]);
-
-	const total = useSignal(true);
-
-	const handleTotalToogle = $((e: boolean) => {
-		skills.value = skills.value.map((skill) => {
-			skill.visible = e;
-			return skill;
-		});
-	});
-
-	const handleSigleToogle = $((id: string, e: boolean) => {
-		const found = skills.value.find((el) => el.id === id);
-		if (found) {
-			found.visible = e;
-		}
-		console.log('#### skills', skills.value);
-		total.value = skills.value.some((skill) => skill.visible);
-	}); */
 
 	return (
 		<>
@@ -41,35 +18,6 @@ export const CompanySkills = component$<Props>(({ company, updateSkillVisibility
 				</span>
 			</div>
 			<div class='flex flex-col sm:space-y-4 md:gap-5 lg:grid lg:grid-cols-2 lg:gap-5'>
-				{/* <ToggleSwitch
-					key={`id_total`}
-					isChecked={total}
-					onChange$={(e: boolean) => {
-						handleTotalToogle(e);
-					}}
-				/>
-				{skills.value.map((skill) =>
-					skill.visible ? (
-						<ToggleSwitch
-							label={skill.visible ? 'Attivo' : 'spento'}
-							id={skill.id}
-							isChecked={true}
-							onChange$={(e: boolean) => {
-								handleSigleToogle(skill.id, e);
-							}}
-						/>
-					) : (
-						<ToggleSwitch
-							label={skill.visible ? 'Attivo' : 'spento'}
-							id={skill.id}
-							isChecked={false}
-							onChange$={(e: boolean) => {
-								handleSigleToogle(skill.id, e);
-							}}
-						/>
-					)
-				)} */}
-
 				<CompanySkillsServiceLine
 					updateSkillVisibility={updateSkillVisibility}
 					_companySkills={_companySkills}
